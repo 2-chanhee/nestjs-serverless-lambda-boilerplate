45ee2a6c79d58714794222ae29f4963b615fd2de
2-chanhee <54311184+2-chanhee@users.noreply.github.com>
2022-03-27 14:15:25 +0900

    serverless + config (#4)

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
+import configuration from '../configuration';
 import { CommonController } from './common.controller';
 import { CommonService } from './common.service';
 
 @Module({
-    imports: [],
+    imports: [
+        /**
+         * validation check
+         * local: .env file (repo에는 commit 안함)
+         * 배포 시: git action에서 .env file 생성하여 사용
+         */
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+            validationSchema: Joi.object({
+                MYSQL_HOST: Joi.string().required(),
+                MYSQL_USERNAME: Joi.string().required(),
+                MYSQL_PASSWORD: Joi.string().required(),
+                MYSQL_DATABASE: Joi.string().required()
+            }),
+            load: [configuration]
+        })
+    ],
     controllers: [CommonController],
     providers: [CommonService]
 })
